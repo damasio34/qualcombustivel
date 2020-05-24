5aae63c93f581ae92af90b3309f31f1a56ba6c0c
Darlan Damasio <damasio34@gmail.com>
2020-05-24 00:46:40 -0300

    Aplicando a máscara no input

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
 import 'package:qualcombustivel/widgets/logo.widget.dart';
 
 void main() {
@@ -22,13 +23,28 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  var _gasCtrl = new MoneyMaskedTextController();
+  var _alcCtrl = new MoneyMaskedTextController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor, //Obtendo cor primária
       body: ListView(
         children: <Widget>[
-          Logo()
+          Logo(),
+          TextFormField(
+            controller: _gasCtrl,
+            keyboardType: TextInputType.number,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 45,
+              fontFamily: "Big Shoulders Display"
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none
+            ),
+          )
         ],
       )
     );
