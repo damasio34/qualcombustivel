0e251f8677804b493f9d4b800dca329b8b167ee9
Darlan Damasio <damasio34@gmail.com>
2020-05-24 01:47:49 -0300

    Componentizando a janela de sucesso

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_masked_text/flutter_masked_text.dart';
 import 'package:qualcombustivel/widgets/input.widget.dart';
 import 'package:qualcombustivel/widgets/loading-button.widget.dart';
 import 'package:qualcombustivel/widgets/logo.widget.dart';
+import 'package:qualcombustivel/widgets/success.widget.dart';
 
 void main() {
   runApp(MyApp());
@@ -35,37 +36,9 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           Logo(),
-          Container(
-            margin: EdgeInsets.all(30),
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(25)
-            ),
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 50,
-                ),
-                Text(
-                  "Compensa utilizar Álcool",
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontSize: 40,
-                    fontFamily: "Big Shoulders Display"
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                LoadingButton(
-                  busy: false, 
-                  func: (){},
-                  text: "CALCULAR NOVAMENTE",
-                  invert: true
-                ),
-              ],
-            ),
+          Success(
+            result: "Compensa utilizar X",
+            reset: () {}
           ),
           Input(
             ctrl: this._gasCtrl,
